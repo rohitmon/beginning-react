f82c7a2c7c08204b6d4d686e4a3e67905297ad89
Mondal Rohit (IFMSIN IT RDI ADS WEB) <rohit.mondal@infineon.com>
2024-11-26 11:39:48 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [title, setTitle] = useState("");
+
+  const getData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const task = await response.json();
+    console.log(task);
+    setTitle(task.title);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h1>{title}</h1>
+    </>
+  );
+}
+
+export default App;
